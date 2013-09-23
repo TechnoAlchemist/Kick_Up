9695d513dd71d68cfbc8e822d4c50c70ae3157ce
Gabriel Lafontant <glafonta@gmail.com>
2013-09-23 15:22:29 -0400

    created user signs in

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature 'user signs up', %q{
+As a user
+I want to enter my account information
+So I can brag about my club
+
+
+} do
+  
+
+
+# Acceptance Criteria:
+# *User is brought to a page where he needs to enter username and password or email address and password
+# * The user is then brought to personalize homepage
+  let(:user) {FactoryGirl.create(:user)}
+  scenario 'user signs in' do
+    sign_in_as(user)
+    expect(page).to have_content("Welcome #{user.email}")
+  
+  end
+end
